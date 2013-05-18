9032ae0b940909bc4a2ccf9860cd90e3d3410e08
Shreyas Ubale <shreyasubale@amistype.com>
2013-05-18 10:01:24 +0530

    Added Packages, shrinkwrap and modified for new version of node

--- server.js
+++ server.js
@@ -10,28 +10,26 @@ var formidable = require('formidable');
 var fileServer = new staticServer.Server(__dirname + '/public');
 var PORT = 8085;
 
-var server = http.createServer(function(request, response){
-    if (request.url === '/upload'){
+var server = http.createServer(function(request, response) {
+    if (request.url === '/upload') {
         var form = new formidable.IncomingForm();
-        form.parse(request, function(err, fields, files){
-            medialist.saveMedia(files, function(){
-                websock.sockets.in('users').emit('medialist updated', medialist.list);
+        form.parse(request, function(err, fields, files) {
+            medialist.saveMedia(files, function() {
+                websock.sockets. in ('users').emit('medialist updated', medialist.list);
             });
             response.end('ok');
         });
     }else{
-        request.addListener('end', function(){
-            fileServer.serve(request, response);
-        });
+        fileServer.serve(request, response);
     }
 });
 server.listen(PORT);
 
 var websock = socketio.listen(server);
-websock.configure(function(){
+websock.configure(function() {
     websock.disable('log');
 });
-websock.sockets.on('connection', function(socket){
+websock.sockets.on('connection', function(socket) {
     socket.join('users');
 
     socket.emit('init', {
@@ -39,23 +37,22 @@ websock.sockets.on('connection', function(socket){
         medialist: medialist.list,
     });
 
-    socket.on('song selected', function(song){
+    socket.on('song selected', function(song) {
         var time = playlist.addSong(song, 'local', socket.handshake.address.address);
-        if (time === 0){
-            if (playlist.list.length === 1){
+        if (time === 0) {
+            if (playlist.list.length === 1) {
                 playlist.play();
             }
             socket.broadcast.emit('playlist updated', playlist.list);
             socket.emit('playlist updated', playlist.list);
-        }else{
+        } else {
             socket.emit('wait for song add', time);
         }
     });
 });
 
-playlist.onCurrentSongComplete = function(){
-    websock.sockets.in('users').emit('playlist updated', playlist.list);
+playlist.onCurrentSongComplete = function() {
+    websock.sockets. in ('users').emit('playlist updated', playlist.list);
 };
 
 console.log('Server started on port: ' + PORT);
-
