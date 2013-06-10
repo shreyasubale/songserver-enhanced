0a6800feaf729c143b624d24c42741cf113e2965
rijunazar <rinzar@gmail.com>
2013-06-10 11:40:22 +0530

    config changes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 
 module.exports = {
-    songSelectionGap: 2, // minimum time(seconds) user must wait before queuing next song
-    maxSongLimitPerIP: 20 // maximum songs a user can queue in playlist
+    songSelectionGap: 5, // minimum time(seconds) user must wait before queuing next song
+    maxSongLimitPerIP: 5 // maximum songs a user can queue in playlist
 };
